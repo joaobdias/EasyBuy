e43a00b66d1f3875284b47ec92f466e86133617a
joaobdias <85759847+joaobdias@users.noreply.github.com>
2021-07-01 17:07:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,105 @@
+// Open and Close Menu
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+// Close Menu After Click
+const links = document.querySelectorAll('nav ul li a')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/*Change Header after Scroll*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+function scrollHeader() {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+// Swipper Testimonials
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+/*Scroll Review*/
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+#home .text, #home .image,
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials,
+#contact .text, #contact .links,
+footer .brand, footer .social`,
+  { interval: 100 }
+)
+
+/*Back to Top*/
+const backToTopButton = document.querySelector('.back-to-top')
+function backTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+// Change Menu Active
+/*const sections = document.querySelectorAll('section[id]')
+function menuCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      documento
+        .querySelector('nav ul li a[href]*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      documento
+        .querySelector('nav ul li a[href]*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}*/
+
+/*Functions*/
+window.addEventListener('scroll', function () {
+  scrollHeader()
+  backTop()
+  //  menuCurrentSection()
+})
